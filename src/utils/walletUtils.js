0a27c726726a601c5f1bf3d2b89651025e77837e
ankitn0 <ankit.an.namdeo@gmail.com>
2025-04-14 18:46:34 +0530

    comment line

--- src/utils/walletUtils.js
+++ src/utils/walletUtils.js
@@ -5,6 +5,7 @@ import { wordlist } from "@scure/bip39/wordlists/english";
 export async function generateWallet() {
   const mnemonic = bip39.generateMnemonic(wordlist);
   const wallet = ethers.Wallet.fromPhrase(mnemonic);
+  //co
   return {
     mnemonic,
     address: wallet.address,
